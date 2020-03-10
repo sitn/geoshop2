241c4300981849756ac314fe461da97f84de8060
mmi <mmi@arxit.com>
2020-03-10 14:47:13 +0100

    Front: map's buttons

--- front/src/app/material-module.ts
+++ front/src/app/material-module.ts
@@ -3,7 +3,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
 
 const MODULES = [
     MatSidenavModule,
@@ -11,6 +15,10 @@ const MODULES = [
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
 ];
 
 @NgModule({
--- front/src/app/welcome/map/map.component.ts
+++ front/src/app/welcome/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from '../services/map/map.service';
+import { IBasemap } from 'src/app/_models/IConfig';
 
 @Component({
   selector: 'gs2-map',
@@ -8,10 +9,33 @@ import { MapService } from '../services/map/map.service';
 })
 export class MapComponent implements OnInit {
 
+  isDrawing = false;
+  isTracking = false;
+  basemaps: Array<IBasemap>;
+  isMapLoading$ = this.mapService.isMapLoading$;
+  
   constructor(private mapService: MapService) { }
 
   ngOnInit(): void {
     this.mapService.initialize();
+    this.mapService.isTracking$.subscribe((isTracking) => {
+      this.isTracking = isTracking;
+    });
+    this.basemaps = this.mapService.Basemaps;
+  }
+
+  toggleDrawing() {
+    this.isDrawing = !this.isDrawing;
+    this.mapService.toggleDrawing();
+  }
+  eraseDrawing() {
+    this.mapService.eraseDrawing();
+  }
+  switchBasemap(gsId: number) {
+    this.mapService.switchBasemap(gsId);
+  }
+  toggleGeolocation() {
+    this.mapService.toggleTracking();
   }
 
 }
