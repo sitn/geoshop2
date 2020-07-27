9eb2793158f015bad17425f789756666252770db
maltaesousa <stephane.maltaesousa@ne.ch>
2020-07-27 15:59:47 +0200

    add apache config

---
+++ scripts/replace-with-env.ps1
@@ -0,0 +1,22 @@
+$callback = {
+    param($match)
+    $env:PWD = pwd
+    $env_path = "Env:" + $match.Groups[1]
+    If (Test-Path $env_path) {
+        $env_data = Get-Item -Path $env_path
+        return $env_data.Value
+    }
+    return ""
+}
+
+Function Replace-With-Env {
+    param(
+        [Parameter(Mandatory)]
+        [string]$InFile,
+        [Parameter(Mandatory)]
+        [string]$OutFile
+    )
+    $file_content = (Get-Content -path $InFile) -join "`n"
+    $file_content = [regex]::Replace($file_content,'\$\{([A-Z_]+)\}',$callback)
+    [IO.File]::WriteAllLines($OutFile, $file_content)
+}
