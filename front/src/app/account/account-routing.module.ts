0b980e1a22d695edb51c4adf5b2d5db537015d61
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2020-12-08 14:32:36 +0100

    Front end adaptations

--- front/src/app/account/account-routing.module.ts
+++ front/src/app/account/account-routing.module.ts
@@ -5,12 +5,14 @@ import {AccountComponent} from './account.component';
 import {NewOrderComponent} from './new-order/new-order.component';
 import {OrdersComponent} from './orders/orders.component';
 import {ProfileComponent} from './profile/profile.component';
+import {ModifyProfileComponent} from './profile/modify-profile.component';
 
 const routes: Routes = [
   {path: '', component: AccountComponent},
   {path: 'new-order', component: NewOrderComponent},
   {path: 'orders', component: OrdersComponent},
   {path: 'profile', component: ProfileComponent},
+  {path: 'modify-profile', component: ModifyProfileComponent},
 ];
 
 @NgModule({
