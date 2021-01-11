e06176099977d049fa54344f3fb97252bdde3056
maltaesousa <stephane.maltaesousa@ne.ch>
2021-01-11 11:04:23 +0100

    Set JWT as default

--- back/settings.py
+++ back/settings.py
@@ -151,8 +151,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
