f67615971a11749753219447942e7f53243361c9
maltaesousa <stephane.maltaesousa@ne.ch>
2021-01-25 17:17:57 +0100

    hide unused fields in admin

--- back/api/admin.py
+++ back/api/admin.py
@@ -118,6 +118,13 @@ class PricingAdmin(CustomModelAdmin):
 
 class UserAdmin(BaseUserAdmin):
     """Overrides BaseUserAdmin"""
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
     inlines = [IdentityInline]
 
