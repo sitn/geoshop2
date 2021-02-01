debad03ca8bcaad1ffe20d6f889e60fe25df7955
maltaesousa <stephane.maltaesousa@ne.ch>
2021-02-01 18:48:10 +0100

    update deploy scripts

--- scripts/0_create_db.ps1
+++ scripts/0_create_db.ps1
@@ -16,9 +16,4 @@ psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $env:PGDATABASE;"
 psql -U postgres -d postgres -c "CREATE DATABASE $env:PGDATABASE OWNER $env:PGUSER;"
 psql -U postgres -d $env:PGDATABASE -c "CREATE EXTENSION postgis;"
 $env:PGPASSWORD = $geoshop_password
-psql -U $env:PGUSER -d $env:PGDATABASE -c "CREATE SCHEMA $env:PGSCHEMA;"
-psql -U $env:PGUSER -d $env:PGDATABASE -c "CREATE SCHEMA amenagement AUTHORIZATION geoshop;"
-psql -U $env:PGUSER -d $env:PGDATABASE -c "CREATE SCHEMA agriculture AUTHORIZATION geoshop;"
-psql -U $env:PGUSER -d $env:PGDATABASE -c "CREATE SCHEMA dangersnaturels AUTHORIZATION geoshop;"
-psql -U $env:PGUSER -d $env:PGDATABASE -c "CREATE SCHEMA environnement AUTHORIZATION geoshop;"
-psql -U $env:PGUSER -d $env:PGDATABASE -c "CREATE SCHEMA defense_incendie AUTHORIZATION geoshop;"
+psql -U $env:PGUSER -d $env:PGDATABASE -f $PSScriptRoot\schemas.sql
---
+++ scripts/schemas.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA geoshop;
+CREATE SCHEMA amenagement AUTHORIZATION geoshop;
+CREATE SCHEMA agriculture AUTHORIZATION geoshop;
+CREATE SCHEMA dangersnaturels AUTHORIZATION geoshop;
+CREATE SCHEMA environnement AUTHORIZATION geoshop;
+CREATE SCHEMA defense_incendie AUTHORIZATION geoshop;
+CREATE SCHEMA eaux AUTHORIZATION geoshop;
+CREATE SCHEMA forets AUTHORIZATION geoshop;
