86611b1676a35de24233e4b54de4269a99d16d76
maltaesousa <stephane.maltaesousa@ne.ch>
2021-02-26 08:41:52 +0100

    fix database restore

--- scripts/3_deploy_prod_database.ps1
+++ scripts/3_deploy_prod_database.ps1
@@ -1,6 +1,6 @@
 $pwd = pwd
 # Read .env
-$ok = Read-Host -Prompt 'Did you manually ran "\\necarto2\e$\SITN\Projets\Geoshop\02_realisation\10_prod_changes.fmw"? y | n'
+$ok = Read-Host -Prompt 'This will propagate prepub geoshop databas to prod, are you sure? y | n'
 
 foreach ($line in Get-Content $PSScriptRoot\..\back\.env.prod) {
     $args = $line -split "="
@@ -19,12 +19,12 @@ if ($ok -eq 'y') {
     pg_dump -U postgres -F c -b -v --schema=$env:PGSCHEMA -f $prodBackup $env:PGDATABASE
     psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$env:PGDATABASE';"
     psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$env:PGDATABASE';"
-    psql -U postgres -d postgres -c "DROP SCHEMA IF EXISTS old_$env:PGSCHEMA;"
-    psql -U postgres -d postgres -c "ALTER SCHEMA $env:PGSCHEMA RENAME TO old_$env:PGSCHEMA;"
+    psql -U postgres -d $env:PGDATABASE -c "DROP SCHEMA IF EXISTS old_$env:PGSCHEMA;"
+    psql -U postgres -d $env:PGDATABASE -c "ALTER SCHEMA $env:PGSCHEMA RENAME TO old_$env:PGSCHEMA;"
     psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'geoshop2_prepub';"
     psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'geoshop2_prepub';"
     pg_dump -U postgres -F c -b -v --schema=$env:PGSCHEMA -f $tempBackup geoshop2_prepub
-    pg_restore -U postgres -F c -d $env:PGDATABASE $dumpFile
+    pg_restore -U postgres -F c -d $env:PGDATABASE $tempBackup
 }
 
 cd $pwd
