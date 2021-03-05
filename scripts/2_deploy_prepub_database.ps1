46d07fa134064b642281cffe281fbc99d230f235
maltaesousa <stephane.maltaesousa@ne.ch>
2021-03-05 16:07:00 +0100

    database script

--- scripts/2_deploy_prepub_database.ps1
+++ scripts/2_deploy_prepub_database.ps1
@@ -18,15 +18,14 @@ Copy-Item -Recurse $PSScriptRoot\..\back\files\extract\* -Destination "\\$env:PG
 If (Test-Path $dumpFile) {
     $geoshop_password = $env:PGPASSWORD
     $env:PGPASSWORD = $env:PGPOSTGRESPASSWORD
-    # pg_dump -U postgres -F c -b -v --schema=$env:PGSCHEMA -f $dumpFile $env:PGDATABASE
     psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$env:PGDATABASE';"
     psql -U postgres -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$env:PGDATABASE';"
     psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS $env:PGDATABASE;"
     psql -U postgres -d postgres -c "CREATE DATABASE $env:PGDATABASE OWNER $env:PGUSER;"
     psql -U postgres -d $env:PGDATABASE -c "CREATE EXTENSION postgis;"
     pg_restore -U postgres -F c -d $env:PGDATABASE $dumpFile
-    fme $env:FMEDIR\10_prod_changes.fmw
     $env:PGPASSWORD = $geoshop_password
+    fme $env:FMEDIR\10_prod_changes.fmw
     psql -U $env:PGUSER -d $env:PGDATABASE -f $PSScriptRoot\triggers.sql
 } Else {
     Write-Host "$(Get-Date -Format g) pg_restore has not been done"
