821a9a20b70a7315eb9cb7e080725d95462d0015
maltaesousa <stephane.maltaesousa@ne.ch>
2021-04-28 11:54:13 +0200

    only get last_draft if no geometry is drawn

--- front/src/app/_store/auth/auth.effects.ts
+++ front/src/app/_store/auth/auth.effects.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Actions, ofType, createEffect} from '@ngrx/effects';
+import {Store} from '@ngrx/store';
+import {AppState, selectOrder} from '../../_store';
 import * as LoginActions from './auth.action';
 import {catchError, exhaustMap, map, take, tap} from 'rxjs/operators';
 import {ApiService} from '../../_services/api.service';
@@ -9,21 +11,27 @@ import {MatSnackBar, MatSnackBarRef} from '@angular/material/snack-bar';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {ConfirmDialogComponent} from '../../_components/confirm-dialog/confirm-dialog.component';
 import {ApiOrderService} from '../../_services/api-order.service';
+import {IOrder} from '../../_models/IOrder';
 import {StoreService} from '../../_services/store.service';
 
 @Injectable()
 export class AuthEffects {
 
+  order: IOrder;
   private snackBarRef: MatSnackBarRef<any>;
 
-  constructor(
-    private action$: Actions,
-    private apiService: ApiService,
-    private apiOrderService: ApiOrderService,
-    private snackBar: MatSnackBar,
-    private dialog: MatDialog,
-    private storeService: StoreService,
-    private router: Router) {
+  constructor(private store: Store<AppState>,
+              private action$: Actions,
+              private apiService: ApiService,
+              private apiOrderService: ApiOrderService,
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog,
+              private storeService: StoreService,
+              private router: Router
+  ) {
+    this.store.select(selectOrder).subscribe(x => {
+      this.order = x;
+    });
   }
 
   login$ = createEffect(() =>
@@ -64,28 +72,31 @@ export class AuthEffects {
           if (this.snackBarRef) {
             this.snackBarRef.dismiss();
           }
-
-          this.apiOrderService.getLastDraft()
-            .pipe(take(1))
-            .subscribe(order => {
-              if (order) {
-                let dialogRef: MatDialogRef<ConfirmDialogComponent> | null = this.dialog.open(ConfirmDialogComponent, {
-                  disableClose: false,
-                });
-
-                dialogRef.componentInstance.noButtonTitle = 'Ignorer';
-                dialogRef.componentInstance.yesButtonTitle = 'Recharger';
-                dialogRef.componentInstance.confirmMessage = 'Vous avez un panier sauvegardé, voulez-vous le recharger ?';
-                dialogRef.afterClosed().subscribe(result => {
-                  if (result) {
-                    this.storeService.addOrderToStore(order);
-                  }
-                  this.storeService.IsLastDraftAlreadyLoadedOrChecked = true;
-                  dialogRef = null;
-                });
-              }
-              this.router.navigate([payload.callbackUrl || '/']);
-            });
+          if (!this.order.geom) {
+            this.apiOrderService.getLastDraft()
+              .pipe(take(1))
+              .subscribe(order => {
+                if (order) {
+                  let dialogRef: MatDialogRef<ConfirmDialogComponent> | null = this.dialog.open(ConfirmDialogComponent, {
+                    disableClose: false,
+                  });
+  
+                  dialogRef.componentInstance.noButtonTitle = 'Ignorer';
+                  dialogRef.componentInstance.yesButtonTitle = 'Recharger';
+                  dialogRef.componentInstance.confirmMessage = `
+                    Vous aviez une commande non finalisée dans votre panier la dernière fois que vous vous êtes déconnecté,
+                    voulez-vous la recharger?`;
+                  dialogRef.afterClosed().subscribe(result => {
+                    if (result) {
+                      this.storeService.addOrderToStore(order);
+                    }
+                    this.storeService.IsLastDraftAlreadyLoadedOrChecked = true;
+                    dialogRef = null;
+                  });
+                }
+              });
+            }
+          this.router.navigate([payload.callbackUrl || '/']);
         })
       ), {
       dispatch: false
