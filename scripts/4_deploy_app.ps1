9739eaca8c6236792bd1780171c208a4faa01b4a
maltaesousa <stephane.maltaesousa@ne.ch>
2021-10-08 08:39:14 +0200

    check debug

--- scripts/4_deploy_app.ps1
+++ scripts/4_deploy_app.ps1
@@ -1,4 +1,10 @@
 . "$PSScriptRoot\replace-with-env.ps1"
+$settings = Get-Content ("{0}\..\back\settings.py" -f $PSScriptRoot)
+$isDebug = $settings | Select-String -Pattern "^\DEBUG = (True)$"
+if ($isDebug.Matches.Success) {
+    Write-Output "Cannot deploy if DEBUG=True in settings.py"
+    Exit
+}
 $destConfig = Read-Host -Prompt 'Input "prod", "prepub" or "dev"'
 $envFile = ("{0}\..\back\.env.{1}" -f $PSScriptRoot, $destConfig)
 # Read .env
